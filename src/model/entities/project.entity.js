3fc00b762d343a7ea9620aa92ba79acc78826450
Jairo Lopez Londoño <31752670+jairolopezlon@users.noreply.github.com>
2021-12-08 07:57:45 -0500

    Update schema and model (#26)
    * se agrega historia 13 y 14
    
    * update entities
    
    * added admin role like environment variable
    
    * updated schema with model

--- src/model/entities/project.entity.js
+++ src/model/entities/project.entity.js
@@ -1,128 +1,152 @@
-module.exports = mongoose => {
-  var schema = mongoose.Schema({
-    title: {
-      type: String,
-      required: true
-    },
-    generalObjective: {
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        index: true,
-        required: true,
-        auto: true,
-      },
-      title: {
-        type: String,
-        required: true
-      },
-      accomplished: {
-        type: Boolean,
-        required: true,
-        default: false
-      }
-    },
-    specificObjectives: [{
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        index: true,
-        required: true,
-        auto: true,
-      },
-      title: {
-        type: String,
-        required: true
-      },
-      accomplished: {
-        type: Boolean,
-        required: true,
-        default: false
-      }
-    }],
-    stage: {
-      type: String,
-      enum: ['pendingApproval', 'approved', 'inProgress', 'cancelled', 'suspended', 'inReview', 'completed'],
-      required: true,
-      trim: true,
-      default: 'pendingApproval'
-    },
-    startDate: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    finishDate: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    budget: {
-      type: Number,
-      required: true,
-      trim: true,
-      default: 0
-    },
-    detailsBudget: [{
-      reason: {
-        type: String,
-        required: true
-      },
-      amount: {
-        type: Number,
-        required: true,
-        trim: true,
-        default: 0
-      }
-    }],
-    leaderInChange: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
-    },
-    studentMembers: [{
-      student: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      },
-      accepted: {
-        type: Boolean,
-        default: false
-      }
-    }],
-    progress: [{
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        index: true,
-        required: true,
-        auto: true,
-      },
-      student: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-      },
-      description: {
-        type: String,
-        required: true
-      },
-      observation: {
-        type: String,
-        required: true
-      }
-    }],
-  }, {
-    timestamps: true
-  });
+module.exports = (mongoose) => {
+    var schema = mongoose.Schema(
+        {
+            title: {
+                type: String,
+                required: true,
+            },
+            generalObjective: {
+                type: String,
+                required: true,
+            },
+            specificObjectives: [
+                {
+                    _id: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        index: true,
+                        required: true,
+                        auto: true,
+                    },
+                    title: {
+                        type: String,
+                        required: true,
+                    },
+                    accomplished: {
+                        type: Boolean,
+                        required: true,
+                        default: false,
+                    },
+                },
+            ],
+            budget: {
+                type: Number,
+                required: true,
+                trim: true,
+                default: 0,
+            },
+            status: {
+                type: String,
+                enum: ['activo', 'inactivo'],
+                required: true,
+                trim: true,
+                default: 'inactivo',
+            },
+            stage: {
+                type: String,
+                enum: ['iniciado', 'en desarrollo', 'terminado'],
+                required: true,
+                trim: true,
+                default: null,
+            },
+            startDate: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            finishDate: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            leaderInCharge: {
+                _id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'Users',
+                },
+                fullName: {
+                    type: String,
+                    required: true,
+                },
+                documentId: {
+                    type: String,
+                    required: true,
+                },
+            },
+            studentMembers: [
+                {
+                    _id: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        required: true,
+                        ref: 'Users',
+                    },
+                    fullName: {
+                        type: String,
+                        required: true,
+                    },
+                    inscriptionStatus: {
+                        type: String,
+                        enum: ['aceptada', 'rechazada'],
+                        required: true,
+                        trim: true,
+                        default: null,
+                    },
+                    dateOfAdmission: {
+                        type: String,
+                        trim: true,
+                        default: null,
+                    },
+                    egressDate: {
+                        type: String,
+                        trim: true,
+                        default: null,
+                    },
+                },
+            ],
+            progress: [
+                {
+                    _id: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        index: true,
+                        required: true,
+                        auto: true,
+                    },
+                    studentId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        required: true,
+                        ref: 'Users',
+                    },
+                    fulstudentFullNamelName: {
+                        type: String,
+                        required: true,
+                    },
+                    description: {
+                        type: String,
+                        required: true,
+                    },
+                    createdDate: {
+                        type: String,
+                        trim: true,
+                        default: new Date(),
+                    },
+                    observation: {
+                        type: String,
+                        default: null,
+                    },
+                },
+            ],
+        },
+        {
+            timestamps: true,
+        },
+    );
 
-  schema.method("toJSON", function () {
-    const {
-      __v,
-      _id,
-      ...object
-    } = this.toObject();
-    object.id = _id.toHexString();
-    return object;
-  });
+    schema.method('toJSON', function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id.toHexString();
+        return object;
+    });
 
-  const Project = mongoose.model("project", schema);
-  return Project;
+    const Project = mongoose.model('project', schema);
+    return Project;
 };
